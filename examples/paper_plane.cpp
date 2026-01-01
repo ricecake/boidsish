bda82c44ab2e30c53bd36a3e4ac2f17967d1f0eb
ricecake <ricecake@greenstuff.io>
2025-12-31 23:24:08 -0500

    tweaks

--- examples/paper_plane.cpp
+++ examples/paper_plane.cpp
@@ -175,7 +175,7 @@ public:
 			handler.QueueAddEntity<CatMissile>(
 				GetPosition(),
 				orientation_,
-				ObjectToWorld(glm::vec3(0, -1, 0)),
+				orientation_ * glm::vec3(0, -1, 0),
 				GetVelocity()
 			);
 		}
