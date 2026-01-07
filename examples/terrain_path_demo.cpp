b35a715a372a7a93b38d504713aae99b250fcf55
ricecake <ricecake@greenstuff.io>
2026-01-07 11:54:11 -0500

    wip

--- examples/terrain_path_demo.cpp
+++ examples/terrain_path_demo.cpp
@@ -2,7 +2,10 @@
 
 #include "dot.h"
 #include "graphics.h"
+#include "path.h"
 #include "terrain_generator.h"
+
+using namespace Boidsish;
 
 int main(int argc, char** argv) {
 	Boidsish::Visualizer visualizer(1024, 768, "Terrain Path Demo");
@@ -10,23 +13,20 @@ int main(int argc, char** argv) {
 	// Get the terrain generator from the visualizer
 	const Boidsish::TerrainGenerator* terrain_generator = visualizer.GetTerrainGenerator();
 
-	if (terrain_generator) {
 		// Get the path
-		auto path = terrain_generator->GetPath({0, 0}, 200, 5.0f);
+		auto terrainPath = terrain_generator->GetPath({0, 0}, 200, 5.0f);
+		auto path_handler = std::make_shared<PathHandler>();
+		auto path = path_handler->AddPath();
+		path->SetMode(PathMode::LOOP);
+		std::vector<std::shared_ptr<Boidsish::Shape>> shapes;
+		for (const auto& point : terrainPath) {
+			path->AddWaypoint({point.x, point.y+10, point.z});
+		}
 
 		// Create a shape handler to render the path
-		visualizer.AddShapeHandler([path](float time) {
-			std::vector<std::shared_ptr<Boidsish::Shape>> shapes;
-			for (const auto& point : path) {
-				auto dot = std::make_shared<Boidsish::Dot>();
-				dot->SetPosition(point.x, point.y + 0.2f, point.z);
-				dot->SetColor(1.0f, 0.0f, 0.0f, 1.0f);
-				dot->SetSize(0.5f);
-				shapes.push_back(dot);
-			}
-			return shapes;
-		});
-	}
+		visualizer.AddShapeHandler([&](float time) { return path_handler->GetShapes(); });
+
+		visualizer.SetPathCamera(path);
 
 	visualizer.Run();
 
