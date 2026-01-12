5d95df620817d026786d001b7783f6fad00c26bd
ricecake <ricecake@greenstuff.io>
2026-01-11 22:32:37 -0500

    start of using z curve for better line cloeness stuff

--- examples/superchunk_generator_test/main.cpp
+++ examples/superchunk_generator_test/main.cpp
@@ -5,8 +5,10 @@
 
 int main() {
 	Boidsish::TerrainGenerator terrain_generator;
-	std::vector<uint16_t>      pixels = terrain_generator.GenerateSuperChunkTexture(0, 0);
-	assert(pixels.size() == 128 * 128 * 4);
-	terrain_generator.ConvertDatToPng("terrain_cache/superchunk_0_0.dat", "terrain_cache/superchunk_0_0.png");
+	for (auto i = 0; i < 4 * 32 * 32; i += 32) {
+		std::vector<uint16_t> pixels = terrain_generator.GenerateSuperChunkTexture(0, i);
+	}
+	// assert(pixels.size() == 128 * 128 * 4);
+	// terrain_generator.ConvertDatToPng("terrain_cache/superchunk_0_0.dat", "terrain_cache/superchunk_0_0.png");
 	return 0;
 }
