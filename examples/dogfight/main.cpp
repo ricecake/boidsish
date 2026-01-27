fb005144374793fba6bd59d5a90701b65946bf2c
ricecake <ricecake@greenstuff.io>
2026-01-27 13:38:06 -0500

    dogfights

---
+++ examples/dogfight/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <memory>
+
+#include "DogfightHandler.h"
+#include "graphics.h"
+
+using namespace Boidsish;
+
+int main() {
+	try {
+		auto visualizer = std::make_shared<Visualizer>(1280, 720, "Dogfight Demo");
+
+		auto handler = std::make_shared<DogfightHandler>(visualizer->GetThreadPool(), visualizer);
+		visualizer->AddShapeHandler(std::ref(*handler));
+
+		// Initial camera position
+		auto [h, n] = visualizer->GetTerrainPointProperties(0, 0);
+		Camera camera(0.0f, h + 100.0f, 200.0f);
+		visualizer->SetCamera(camera);
+
+		std::cout << "Dogfight Demo starting..." << std::endl;
+		std::cout << "Red and Blue teams will chase each other and engage in combat." << std::endl;
+		std::cout << "Planes will explode if an enemy stays behind them for too long or if they hit terrain."
+				  << std::endl;
+
+		visualizer->Run();
+	} catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
---
+++ examples/dogfight/src/DogfightHandler.cpp
@@ -0,0 +1,47 @@
+#include "DogfightHandler.h"
+#include "terrain_generator.h"
+
+namespace Boidsish {
+
+	DogfightHandler::DogfightHandler(
+		task_thread_pool::task_thread_pool& thread_pool,
+		std::shared_ptr<Visualizer>         visualizer
+	):
+		SpatialEntityHandler(thread_pool, visualizer) {}
+
+	void DogfightHandler::PreTimestep(float time, float delta_time) {
+		(void)time;
+		(void)delta_time;
+
+		int red_count = 0;
+		int blue_count = 0;
+
+		for (auto& pair : GetAllEntities()) {
+			auto plane = std::dynamic_pointer_cast<DogfightPlane>(pair.second);
+			if (plane) {
+				if (plane->GetTeam() == Team::RED)
+					red_count++;
+				else
+					blue_count++;
+			}
+		}
+
+		const int kMinPlanes = 15;
+		if (red_count < kMinPlanes)
+			SpawnPlane(Team::RED);
+		if (blue_count < kMinPlanes)
+			SpawnPlane(Team::BLUE);
+	}
+
+	void DogfightHandler::SpawnPlane(Team team) {
+		float x = (float)(rand() % 500 - 250);
+		float z = (float)(rand() % 500 - 250);
+
+		// We can't easily get terrain height here if generator isn't ready or visible
+		// but EntityHandler has GetTerrainPointPropertiesThreadSafe
+		auto [h, n] = GetTerrainPointPropertiesThreadSafe(x, z);
+
+		QueueAddEntity<DogfightPlane>(team, Vector3(x, h + 75.0f, z));
+	}
+
+} // namespace Boidsish
