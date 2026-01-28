7cffb8441d14db07d032c65b9cee7d6127f560d3
ricecake <ricecake@greenstuff.io>
2026-01-28 00:07:25 -0500

    notes

--- include/light_manager.h
+++ include/light_manager.h
@@ -30,6 +30,10 @@ namespace Boidsish {
 		// Default light casts shadows
 		std::vector<Light> _lights{Light::CreateDirectional({0, 100, -500}, {0, -1, 1}, 1.0f, {1, 0.5f, 0.25f}, true)};
 		glm::vec3          _ambient_light = glm::vec3(90.0f/255.0f, 81.0f/255.0f, 62.0f/255.0f);
+		/*
+			ambient: 53/58/44
+			def: 231/27/0 @0,100,-100->0,-5.7,7.5 and 6.3 intense
+		*/
 	};
 } // namespace Boidsish
 
