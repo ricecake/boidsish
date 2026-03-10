ef9872d3537104b4977ecb56762fa226b0663261
ricecake <ricecake@greenstuff.io>
2026-03-10 01:12:48 -0400

    spatial particle grid

---
+++ shaders/helpers/spatial_hash.glsl
@@ -0,0 +1,11 @@
+#ifndef SPATIAL_HASH_GLSL
+#define SPATIAL_HASH_GLSL
+
+uint get_cell_idx(vec3 pos, float cellSize, uint gridSize) {
+    ivec3 cellPos = ivec3(floor(pos / cellSize));
+    // Spatial hash using large primes
+    uint h = uint(cellPos.x * 73856093) ^ uint(cellPos.y * 19349663) ^ uint(cellPos.z * 83492791);
+    return h % gridSize;
+}
+
+#endif
