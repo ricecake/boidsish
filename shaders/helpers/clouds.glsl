f4f49be6aed8104709c441e2d70a58805fe6e0ef
ricecake <ricecake@greenstuff.io>
2026-06-15 00:30:50 -0400

    why god why

--- shaders/helpers/clouds.glsl
+++ shaders/helpers/clouds.glsl
@@ -90,7 +90,7 @@ vec3 getWarpedCloudPos(vec3 p, out float fade) {
 }
 
 CloudWeather computeCloudWeather(vec3 p, CloudProperties props) {
-	p += vec3(150 * time, 0, 75 * time);
+	// p += vec3(150 * time, 0, 75 * time);
 	vec2  weatherData = fastWorley3dID(vec3(p.x, 0.0, p.z) / (10000.0 * worldScale));
 	float weatherMap = 1.0 - weatherData.x; // Worley distance for coverage
 	float cellID = weatherData.y;           // Cell ID for variety
@@ -126,10 +126,10 @@ vec3 getCloudAdvectionOffset(float h, float worldScale, float time) {
 	vec2  flowDir = vec2(cos(angle), sin(angle));
 
 	// Dramatic non-linear shear profile
-	float shear = h * h * cloudFlowHeightScale * 2.0;
+	// float shear = h * h * cloudFlowHeightScale * 2.0;
 
 	vec3 advect = vec3(flowDir.x, 0.0, flowDir.y) * time * cloudFlowSpeed * worldScale * 10.0;
-	advect.xz += flowDir * shear * worldScale * 100.0;
+	// advect.xz += flowDir * shear * worldScale * 10.0;
 
 	return advect;
 }
@@ -245,7 +245,8 @@ float calculateCloudDensity(
 	float density = smoothstep(coverageThreshold, max(1.0, coverageThreshold), baseDensity);
 
 	float val = smoothstep(0.0, 1.0, density * densityProfile * props.densityBase * 5.0);
-	return step(0.25, val) * val;
+	return val;
+	// return step(0.25, val) * val;
 }
 
 float calculateCloudShadowDensity(vec3 p, CloudWeather weather, CloudLayer layer, CloudProperties props, float time) {
